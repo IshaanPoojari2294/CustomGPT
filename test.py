2f73570d2fb42fa6e5b64e2bc86eba0e55ad5e79
IshaanPoojari2294 <ip2294@nyu.edu>
2025-02-09 11:20:17 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+
+
+text = "Hi Botnik"
+text_set = set(text)
+text_list = list(text)
+
+print(text_set)
+print(text_list)
+print("H" in text_set)
+{'t', 'k', ' ', 'i', 'o', 'H', 'n', 'B'}
+
+
+
+print("H" in text_list)
+
+['H', 'i', ' ', 'B', 'o', 't', 'n', 'i', 'k']
+
